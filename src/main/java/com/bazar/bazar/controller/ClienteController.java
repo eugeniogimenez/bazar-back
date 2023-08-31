69781141481f6bb8c0ad5adb258aa969ad8763ca
Eugenio Gimenez <eugeniolgimenez@gmail.com>
2023-08-31 16:59:14 -0300

    otro commit

--- src/main/java/com/bazar/bazar/controller/ClienteController.java
+++ src/main/java/com/bazar/bazar/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.bazar.bazar.model.Cliente;
 import com.bazar.bazar.service.IClienteService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     //EL CONTROLLER LLAMA AL SERVICE(Logica)
--- src/main/java/com/bazar/bazar/controller/VentaController.java
+++ src/main/java/com/bazar/bazar/controller/VentaController.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class VentaController {
 
     //EL CONTROLLER LLAMA AL SERVICE(Logica)
