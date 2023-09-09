ea37bca2f4dea80f595368b21e34024acb0618a5
Eugenio Gimenez <eugeniolgimenez@gmail.com>
2023-09-09 08:48:18 -0300

    path index.html

--- src/main/java/com/bazar/bazar/BazarApplication.java
+++ src/main/java/com/bazar/bazar/BazarApplication.java
@@ -2,12 +2,19 @@ package com.bazar.bazar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 public class BazarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BazarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BazarApplication.class, args);
+    }
+
+    @RequestMapping(value = "/{path:[^\\.]*}", method = RequestMethod.GET)
+    public String forward() {
+        return "forward:/index.html";
+    }
 
 }
